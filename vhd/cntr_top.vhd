5cc84f3cc84a073214a9c093613aa479b3e26d20
Stefan Kos <Stefan.Kos@racing.tuwien.ac.at>
2026-06-05 21:06:39 +0200

    Pre Testing State

--- vhd/cntr_top.vhd
+++ vhd/cntr_top.vhd
@@ -21,31 +21,33 @@ use ieee.std_logic_1164.all;
 entity cntr_top is
 	port(
 	clk_i : in std_logic;								-- System Clock (100MHz)
-	reset_i : in std_logic;							 	-- Asynchronous high active reset
+	reset_i : in std_logic;							-- Asynchronous high active reset
+	
 	sw_i : in std_logic_vector(15 downto 0);			-- 16 switches
-	pb_i : in std_logic_vector(3 downto 0);				--4 push buttons
+	pb_i : in std_logic_vector(3 downto 0);			-- 4 push buttons
+	
 	ss_o : out std_logic_vector(7 downto 0); 			-- Contains the value for all four 7 segment digits
-	ss_sel_o : out std_logic_vector(3 downto 0); 		--Selects one out of the four 7-segment digits
-	led_o : out std_logic_vector(15 downto 0) 			--16 leds
+	ss_sel_o : out std_logic_vector(3 downto 0); 		-- Selects one out of the four 7-segment digits
+	led_o : out std_logic_vector(15 downto 0) 			-- 16 leds
 	);
 end entity cntr_top;
 
 architecture rtl of cntr_top is
 	
 	component io_ctrl
-	generic(
-	n : integer := 8;
-	);
 	port(
 	clk_i : in std_logic;							-- System Clock (100MHz)
-	reset_i : in std_logic;							-- Asynchronous high active reset
-	cntr0_i : in std_logic_vector(n downto 0);		-- Digit 0 (from FPGA internal logic)
-	cntr1_i : in std_logic_vector(n downto 0);		-- Digit 1 (from FPGA internal logic)
-	cntr2_i : in std_logic_vector(n downto 0);		-- Digit 2 (from FPGA internal logic)
-	cntr3_i : in std_logic_vector(n downto 0);		-- Digit 3 (from FPGA internal logic)
+	reset_i : in std_logic;						-- Asynchronous high active reset
+	
+	cntr0_i : in std_logic_vector(3 downto 0);	-- Digit 0 (from FPGA internal logic)
+	cntr1_i : in std_logic_vector(3 downto 0);	-- Digit 1 (from FPGA internal logic)
+	cntr2_i : in std_logic_vector(3 downto 0);	-- Digit 2 (from FPGA internal logic)
+	cntr3_i : in std_logic_vector(3 downto 0);	-- Digit 3 (from FPGA internal logic)
+	
 	led_i : in std_logic_vector(15 downto 0);		-- State of 16 LEDs (from FPGA internal logic)
 	sw_i : in std_logic_vector(15 downto 0);		-- 16 switches (from FPGA board)
-	pb_i : in std_logic_vector(4 downto 0);			-- 4 push buttons (from FPGA board)
+	pb_i : in std_logic_vector(3 downto 0);		-- 4 push buttons (from FPGA board)
+	
 	ss_o : out std_logic_vector(7 downto 0);		-- to 7-segment digits of the FPGA board
 	ss_sel_o : out std_logic_vector(3 downto 0);	-- Selection of a 7 segment digit
 	swsync_o : out std_logic_vector(15 downto 0);	-- 16 switches (to FPGA interal logic)
@@ -55,26 +57,74 @@ architecture rtl of cntr_top is
 	end component io_ctrl;
 	
 	component cntr
-	generic(
-	n : integer := 8;
-	);
 	port(
-	clk_i : in std_logic;							-- System Clock (100MHz)
-	reset_i : in std_logic;							-- Asynchronous high active reset
-	cntrup_i : in std_logic;						-- Counts up if signal is '1'
-	cntrdown_i : in std_logic;						-- Counts down if signal is '1'
-	cntrclear_i : in std_logic;						-- Sets counter to 0x0 is signal is '1'
-	cntrhold_i : in std_logic;						-- Holds count value if signal is '1'
-	cntr0_i : out std_logic_vector(n downto 0);		-- Digit 0 (from FPGA internal logic)
-	cntr1_i : out std_logic_vector(n downto 0);		-- Digit 1 (from FPGA internal logic)
-	cntr2_i : out std_logic_vector(n downto 0);		-- Digit 2 (from FPGA internal logic)
-	cntr3_i : out std_logic_vector(n downto 0);		-- Digit 3 (from FPGA internal logic)
+		clk_i : in std_logic;						-- System Clock (100MHz)
+		reset_i : in std_logic;					-- Asynchronous high active reset
+		
+		cntrup_i : in std_logic;					-- Counts up if signal is '1'
+		cntrdown_i : in std_logic;					-- Counts down if signal is '1'
+		cntrclear_i : in std_logic;					-- Sets counter to 0x0 is signal is '1'
+		cntrhold_i : in std_logic;					-- Holds count value if signal is '1'
+		
+		cntr0_o : out std_logic_vector(3 downto 0);	-- Digit 0 (from FPGA internal logic) LS
+		cntr1_o : out std_logic_vector(3 downto 0);	-- Digit 1 (from FPGA internal logic)
+		cntr2_o : out std_logic_vector(3 downto 0);	-- Digit 2 (from FPGA internal logic)
+		cntr3_o : out std_logic_vector(3 downto 0);	-- Digit 3 (from FPGA internal logic) MS
 	);
 	end component cntr;
 	
+	signal swsync : std_logic_vector(15 downto 0);
+	signal pbsync : std_logic_vector(3 downto 0);
+	
+	signal cntr0_s : std_logic_vector(3 downto 0);
+	signal cntr1_s : std_logic_vector(3 downto 0);
+	signal cntr2_s : std_logic_vector(3 downto 0);
+	signal cntr3_s : std_logic_vector(3 downto 0);
+	
+	signal cntr_up_s : std_logic;
+	signal cntr_down_s : std_logic;
+	signal cntr_clear_s : std_logic;
+	signal cntr_hold_s : std_logic;
 begin
 
+	-- IO Control block
+	u_io_ctrl : io_ctrl
+		port map(
+		clk_i => clk_i,
+		reset_i => reset_i,
+		cntr0_i => cntr0_s,
+		cntr1_i => cntr1_s,
+		cntr2_i => cntr2_s,
+		cntr3_i => cntr3_s,
+		led_i => (others =>'0'),
+		sw_i => sw_i,
+		pb_i => pb_i,
+		ss_o => ss_o,
+		ss_sel_o => ss_sel_o,
+		swsync_o => swsync,
+		pbsync_o => pbsync,
+		led_o => led_o
+		);
+	
+	-- Switch decoding
+	cntr_clear_s <= swsync(3);
+	cntr_hold_s <= not swsync(0);
+	cntr_up_s <= swsync(0) and swsync(1) and not swsync(2) and not swsync(3);
+	cntr_down_s <= swsync(0) and swsync(2) and not swsync(1) and not swsync(3);	
 
-
-
+	-- Counter Block
+	u_cntr : cntr
+		port map(
+		clk_i => clk_i,
+		reset_i => reset_i,
+		cntrup_i => cntr_up_s,
+		cntrdown_i => cntr_down_s,
+		cntrclear_i => cntr_clear_s,
+		cntrhold_i => cntr_hold_s,
+		cntr0_o => cntr0_s,
+		cntr1_o => cntr1_s,
+		cntr2_o => cntr2_s,
+		cntr3_o => cntr3_s
+		);
+		
 end architecture rtl;
--- vhd/io_ctrl.vhd
+++ vhd/io_ctrl.vhd
@@ -22,19 +22,19 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity io_ctrl is
-	generic(
-	n : integer := 8;
-	);
 	port(
 	clk_i : in std_logic;							-- System Clock (100MHz)
-	reset_i : in std_logic;							-- Asynchronous high active reset
-	cntr0_i : in std_logic_vector(n downto 0);		-- Digit 0 (from FPGA internal logic)
-	cntr1_i : in std_logic_vector(n downto 0);		-- Digit 1 (from FPGA internal logic)
-	cntr2_i : in std_logic_vector(n downto 0);		-- Digit 2 (from FPGA internal logic)
-	cntr3_i : in std_logic_vector(n downto 0);		-- Digit 3 (from FPGA internal logic)
+	reset_i : in std_logic;						-- Asynchronous high active reset
+	
+	cntr0_i : in std_logic_vector(3 downto 0);	-- Digit 0 (from FPGA internal logic)
+	cntr1_i : in std_logic_vector(3 downto 0);	-- Digit 1 (from FPGA internal logic)
+	cntr2_i : in std_logic_vector(3 downto 0);	-- Digit 2 (from FPGA internal logic)
+	cntr3_i : in std_logic_vector(3 downto 0);	-- Digit 3 (from FPGA internal logic)
+	
 	led_i : in std_logic_vector(15 downto 0);		-- State of 16 LEDs (from FPGA internal logic)
 	sw_i : in std_logic_vector(15 downto 0);		-- 16 switches (from FPGA board)
-	pb_i : in std_logic_vector(4 downto 0);			-- 4 push buttons (from FPGA board)
+	pb_i : in std_logic_vector(3 downto 0);		-- 4 push buttons (from FPGA board)
+	
 	ss_o : out std_logic_vector(7 downto 0);		-- to 7-segment digits of the FPGA board
 	ss_sel_o : out std_logic_vector(3 downto 0);	-- Selection of a 7 segment digit
 	swsync_o : out std_logic_vector(15 downto 0);	-- 16 switches (to FPGA interal logic)
@@ -45,9 +45,127 @@ end entity io_ctrl;
 
 architecture rtl of io_ctrl is
 
+	-- Frequenz Teiler 1kHz auf 100MHz -> 100_000
+	constant CLK_REFRESH_MAX : unsigned(16 downto 0) := to_unsigned(99_999, 17);
+
+	signal swsync_0, swsync_1 : std_logic_vector(15 downto 0);
+	signal pbsync_0, pbsync_1 : std_logic_vector(3 downto 0);
+	
+	signal refresh_cnt : unsigned(16 downto 0);
+	signal refresh_en : std_logic;
+	
+	signal digit_sel : unsigned (1 downto 0);
+	signal cur_digit : std_logic_vector(3 downto 0);
+	signal ss_data : std_logic_vector(7 downto 0);
+	
 begin
+	
+	-- Synchronisation der Switches und Buttons of clk_i
+	p_sync : process(clk_i, reset_i)
+	begin
+		if reset_i = '1' then
+			swsync_0 <= (others => '0');
+			swsync_1 <= (others => '0');
+			pbsync_0 <= (others => '0');
+			pbsync_1 <= (others => '0');
+		elsif rising_edge(clk_i) then
+			swsync_0 <= sw_i;
+			swsync_1 <= swsync_0;
+			
+			pbsync_0 <= pb_i;
+			pbsync_1 <= pbsync_0;
+		end if;
+	end process p_sync;
+	
+	swsync_o <= swsync_1;
+	pbsync_o <= pbsync_1;
+	
+	led_o <= led_i;
+	
+	-- 1kHz-Refresh Enable
+	p_refresh_div : process(clk_i, reset_i)
+	begin
+		if reset_i = '1' then
+			refresh_cnt <= (others => '0');
+			refresh_en <= '0';
+			digit_sel <= (others => '0');
+		
+		elsif rising_edge(clk_i) then
+			if refresh_cnt = CLK_REFRESH_MAX then
+				refresh_cnt <= (others => '0');
+				refresh_en <= '1';
+			
+			else
+				refresh_cnt <= refresh_cnt + 1;
+				refresh_en <= '0';
+				
+			end if;
+			
+			if refresh_en = '1' then
+				digit_sel <= digit_sel + 1;
+			end if;
+		end if;
+	end process p_refresh_div;
 
+	-- Digit-Multiplexer und Digit-Select (active low)
+	p_digit_mux : process(digit_sel, cntr0_i, cntr1_i, cntr2_i, cntr3_i)
+	begin
+		case digit_sel is
+			when "00" =>
+				cur_digit <= cntr0_i;
+				ss_sel_o <= "1110"; -- Digit 0 aktiv
+			when "01" =>
+				cur_digit <= cntr1_i;
+				ss_sel_o <= "1101";
+			when "10" =>
+				cur_digit <= cntr2_i;
+				ss_sel_o <= "1011";
+			when "11" =>
+				cur_digit <= cntr3_i;
+				ss_sel_o <= "0111";
+		end case;
+	end process p_digit_mux;
+	
+	p_7seg : process(cur_digit) 
+	begin
+		case cur_digit is
+			-- 0
+			when "0000" =>
+				ss_data <= "11000000";
+			
+			-- 1
+			when "0001" =>
+				ss_data <= "11111001";
+	
+			-- 2
+			when "0010" =>
+				ss_data <= "10100100";
+				
+			-- 3
+			when "0011" =>
+				ss_data <= "10110000";
+				
+			-- 4
+			when "0100" =>
+				ss_data <= "10011001";
 
+			-- 5
+			when "0101" =>
+				ss_data <= "10010010";
 
+			-- 6
+			when "0110" =>
+				ss_data <= "10000010";
 
+			-- 7
+			when "0111" =>
+				ss_data <= "11111000";
+
+			when others =>
+				ss_data <= (others => '1');
+		end case;
+	end process p_7seg;
+	
+	ss_o <= ss_data;
+	
 end architecture rtl;
